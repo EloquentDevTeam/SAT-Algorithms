cd493f736903660b64941052e6ebf15d481b5e1e
thonkdifferent <41342923+thonkdifferent@users.noreply.github.com>
2025-04-26 01:51:16 +0300

    Improve handling of zombie processees

--- mkcnf.py
+++ mkcnf.py
@@ -12,12 +12,12 @@ def get_literal(clause_literals: list[int], literal_count: int) -> int:
         if fin_literal not in clause_literals and (-1) * fin_literal not in clause_literals:
             return fin_literal
 
-def get_clauses(literal_count: int, clause_count: int, clause_max_size: int, seed: int | None) -> str:
+def get_clauses(literal_count: int, clause_count: int, clause_min_size: int, clause_max_size: int, seed: int | None) -> str:
     output = StringIO()
     output.write(f'p cnf {literal_count} {clause_count}\n')
     random.seed(seed)
     for c_clause in range(clause_count):
-        clause_size = random.randint(1,clause_max_size)
+        clause_size = random.randint(clause_min_size,clause_max_size)
         literals = list()
         for c_literal in range(clause_size):
             l = get_literal(literals, literal_count)
@@ -36,6 +36,7 @@ if __name__ == '__main__':
 
     parser.add_argument('--clause-count', type=int, required=True, help='How many clauses should be generated')
     parser.add_argument('--clause-max-size',type=int, required= True, help = 'Maximum number of literals a clause can have')
+    parser.add_argument('--clause-min-size',type=int, required=False, default=1, help = 'Minimum number of literals a clause can have')
     parser.add_argument('--literal-count', type=int, required=True, help='How many distinct literals should be in the final formula, bar for their negations')
     parser.add_argument('--batch-size',type=int, default=1, required = False, help='How many tests to generate')
     parser.add_argument('--name',type=str,default='cnf_tests',required=False, help='Name to use for the final folder')
@@ -45,7 +46,12 @@ if __name__ == '__main__':
     parser.add_argument('-f','--force',help='Overwrite existing directory', action='store_true')
 
 
+
     args = parser.parse_args()
+    if args.clause_min_size < 1:
+        print('Clauses must have at least one literal')
+        exit(1)
+
     final_path = f'{args.path}/{args.name}'
     try:
         os.chdir(args.path)
@@ -64,6 +70,6 @@ if __name__ == '__main__':
             exit(0)
     os.chdir(args.name)
     for i in range(args.batch_size):
-        result = get_clauses(args.literal_count,args.clause_count, min(args.clause_max_size,args.literal_count) ,args.seed)
+        result = get_clauses(args.literal_count,args.clause_count,min(args.clause_min_size,args.literal_count), min(args.clause_max_size,args.literal_count) ,args.seed)
         with open(f'test{i+1}.cnf','w') as fwriter:
             fwriter.write(result)
