36375c92de12cab3ece5edabb9916417b3acf546
thonkdifferent <41342923+thonkdifferent@users.noreply.github.com>
2025-04-25 23:02:09 +0300

    Create mkcnf script

---
+++ mkcnf.py
@@ -0,0 +1,56 @@
+import os
+import argparse
+import random
+from io import StringIO
+
+
+def get_clauses(literal_count: int, clause_count: int, clause_max_size: int, seed: int | None) -> str:
+    output = StringIO()
+    output.write(f'p cnf {literal_count} {clause_count}\n')
+    random.seed(seed)
+    for c_clause in range(clause_count):
+        clause_size = random.randint(1,clause_max_size)
+        for c_literal in range(clause_size):
+            polarity = -1 ** random.randint(1,2)
+            literal = random.randint(1,literal_count)
+            fin_literal = literal * polarity
+            output.write(str(fin_literal))
+            output.write(' ')
+        output.write('0')
+        output.write('\n')
+    result = output.getvalue()
+    return result
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(prog="mkcnf.py",
+                                     description="")
+
+    parser.add_argument('--clause-count', type=int, required=True, help='How many clauses should be generated')
+    parser.add_argument('--clause-max-size',type=int, required= True, help = 'Maximum number of literals a clause can have')
+    parser.add_argument('--literal-count', type=int, required=True, help='How many distinct literals should be in the final formula, bar for their negations')
+    parser.add_argument('--batch-size',type=int, default=1, required = False, help='How many tests to generate')
+    parser.add_argument('--name',type=str,default='cnf_tests',required=False, help='Name to use for the final folder')
+    parser.add_argument('--seed',type=int,default=None, required=False, help='Seed for the random number generator to use')
+    parser.add_argument('--path', help='Path to save tests to. A subdirectory will be created there',default=os.getcwd(),required=False)
+
+    args = parser.parse_args()
+    final_path = f'{args.path}/{args.name}'
+    try:
+        os.chdir(args.path)
+        os.mkdir(args.name)
+    except FileExistsError:
+        try:
+            res = input(f'Folder {args.name} already exists in {args.path}. Overwrite? (Y/N)')
+            if not res.startswith('Y') and not res.startswith('y'):
+                print('Aborted.')
+                exit(0)
+            pass
+        except EOFError:
+            print('Aborted.')
+            exit(0)
+    os.chdir(args.name)
+    for i in range(args.batch_size):
+        result = get_clauses(args.literal_count,args.clause_count, args.clause_max_size,args.seed)
+        with open(f'test{i+1}.cnf','w') as fwriter:
+            fwriter.write(result)
