68eb7a2a29b4288a437a0c19b2165ef94569cee2
thonkdifferent <41342923+thonkdifferent@users.noreply.github.com>
2025-05-10 20:38:21 +0300

    Fixed a bunch of issues with the data extraction tool

--- data_sniffer.py
+++ data_sniffer.py
@@ -1,7 +1,7 @@
 import argparse
 import csv
 import os
-
+from decimal import Decimal
 def get_files_in_folder(folder:str):
     l = []
     for item in os.listdir(folder):
@@ -30,8 +30,27 @@ def parse_cnf_txt_result(file: str, number: int):
                 'Algo time': '-1',
                 'Result': 'UNKNOWN'
             }
-        result = {'Test': str(number), 'Result': lines[1].split(':')[1].strip(),
-                  'Max RSS': lines[6].split(':')[1].strip()[:-4], 'Algo time': lines[4].split(':')[1].strip()[:-2]}
+        #if(file == 'tests/batch_4/dpll_mcl_results/test1.cnf.result.txt'):
+        #    breakpoint()
+        max_rss = ''
+        algo_time = ''
+        result = ''
+        if(lines[0].startswith('Start SAT. Result: ')):
+            result = lines[0].split(':')[1].strip()
+            max_rss = lines[4].split(':')[1].strip()[:-3]
+            algo_time = lines[2].split(':')[1].strip()[:-2]
+        elif len(lines) == 7:
+            result = lines[1].split(':')[1].strip()
+            max_rss = lines[4].split(':')[1].strip()[:-3]
+            algo_time = lines[2].split(':')[1].strip()[:-2]
+        else:
+            result = lines[1].split(':')[1].strip()
+            max_rss = lines[6].split(':')[1].strip()[:-3]
+            algo_time = lines[4].split(':')[1].strip()[:-2]
+        result = {'Test': str(number), 'Result': result,
+                  'Max RSS': max_rss, 'Algo time': algo_time}
+        if result['Algo time'].startswith('-') :
+            breakpoint()
         return result
 
 
@@ -51,13 +70,33 @@ if __name__ == "__main__":
     rundir = os.getcwd()
     for folder in files.keys():
         os.chdir(args.test_results_directory)
-        with open(f'{os.path.basename(folder)}.csv','w') as ofobj:
+        with (open(f'{os.path.basename(folder)}.csv','w') as ofobj):
             print(f'Saving to {os.path.basename(folder)}.csv')
             c_writer = csv.writer(ofobj)
             c_writer.writerow(['Test','Max RSS','Algo time', 'Result'])
             lst = []
             os.chdir(rundir)
+            average_algo_time = float(0)
+            average_memory_usage = float(0)
+            succeeded_test = 0
             for file in files[folder]:
                 test_number = int(os.path.basename(file).split('.')[0][4:])
                 test_result  = parse_cnf_txt_result(file,test_number)
                 c_writer.writerow([test_result['Test'],test_result['Max RSS'],test_result['Algo time'], test_result['Result']])
+                c_algo_time = float(test_result['Algo time'])
+                if c_algo_time > 0:
+                    average_algo_time+=c_algo_time
+                    average_memory_usage+=float(test_result['Max RSS'])
+                    succeeded_test+=1
+                if average_algo_time < 0:
+                    breakpoint()
+
+                if average_memory_usage < 0:
+                    breakpoint()
+            if succeeded_test > 0:
+                average_algo_time/=succeeded_test
+                print(f'Average algo time: {round(average_algo_time,3)}')
+                print(f'Average RSS usage: {round(average_memory_usage,3)}')
+            else:
+                print(f'Average algo time: DNF')
+                print(f'Average RSS usage: DNF')
